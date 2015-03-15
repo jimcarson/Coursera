efc43cc29bf6fc378364a96126ea8a3ebfab4726
Jim Carson <jim.carson@gmail.com>
2015-03-15 16:03:13 -0700

    Still struggling with morph.  It works with the doctest in the example as well as the one in the problem set.  Part of the headache has been dealing with lists vs sets

--- hw15_dimension_problems/Dimension_problems.py
+++ hw15_dimension_problems/Dimension_problems.py
@@ -3,22 +3,24 @@ coursera = 1
 # Please fill out this stencil and submit using the provided submission script.
 
 from GF2 import one
-from independence import rank
+from independence import rank, is_independent
 from mat import Mat
-from matutil import listlist2mat, coldict2mat
+from matutil import listlist2mat, coldict2mat, mat2rowdict, mat2coldict
 from solver import solve
 from vec import Vec
 from vecutil import list2vec
+from The_Basis_problems import is_superfluous, exchange, vec2rep
+from triangular import triangular_solve
 
 
 ## 1: (Problem 1) Iterative Exchange Lemma
-w0 = list2vec([1,0,0])
-w1 = list2vec([0,1,0])
-w2 = list2vec([0,0,1])
+w0 = list2vec([1, 0, 0])
+w1 = list2vec([0, 1, 0])
+w2 = list2vec([0, 0, 1])
 
-v0 = list2vec([1,2,3])
-v1 = list2vec([1,3,3])
-v2 = list2vec([0,3,3])
+v0 = list2vec([1, 2, 3])
+v1 = list2vec([1, 3, 3])
+v2 = list2vec([0, 3, 3])
 
 # Fill in exchange_S1 and exchange_S2  IN each, one vector of W is injected and one of V is ejected
 # with appropriate lists of 3 vectors
@@ -29,61 +31,70 @@ exchange_S2 = [v0, v1, w2]
 exchange_S3 = [v0, v1, v2]
 
 ## 2: (Problem 2) Another Iterative Exchange Lemma
-w0 = list2vec([0,one,0])
-w1 = list2vec([0,0,one])
-w2 = list2vec([one,one,one])
+w0 = list2vec([  0, one,   0])
+w1 = list2vec([  0,   0, one])
+w2 = list2vec([one, one, one])
 
-v0 = list2vec([one,0,one])
-v1 = list2vec([one,0,0])
-v2 = list2vec([one,one,0])
+v0 = list2vec([one,   0, one])
+v1 = list2vec([one,   0,   0])
+v2 = list2vec([one, one,   0])
 
 exchange_2_S0 = [w0, w1, w2]
 exchange_2_S1 = [v0, w1, w2]
 exchange_2_S2 = [v0, v1, w2]
 exchange_2_S3 = [v0, v1, v2]
 
-
-
 ## 3: (Problem 3) Morph Lemma Coding
+#
+# There is a disconnect with this in the inputs are lists but the other routines you'd use
+# from last week's homework assume sets.
+#
 def morph(S, B):
     '''
-    Input: S: a list of distinct Vecs, B: a list of linearly independent Vecs all in Span S
+    Input:
+        - S: a list of distinct Vecs
+        - B: a list of linearly independent Vecs all in Span S
     Output: a list of pairs of vectors to inject and eject (see problem description)
-
-    # This is how our morph works.  Yours may yield different results.
-    # Note: Make a copy of S to modify instead of modifying S itself.
-    >>> S = [list2vec(v) for v in [[1,0,0],[0,1,0],[0,0,1]]]
-    >>> B = [list2vec(v) for v in [[1,1,0],[0,1,1],[1,0,1]]]
-    >>> morph(S, B)
-    [(Vec({0, 1, 2},{0: 1, 1: 1, 2: 0}), Vec({0, 1, 2},{0: 1, 1: 0, 2: 0})), (Vec({0, 1, 2},{0: 0, 1: 1, 2: 1}), Vec({0, 1, 2},{0: 0, 1: 1, 2: 0})), (Vec({0, 1, 2},{0: 1, 1: 0, 2: 1}), Vec({0, 1, 2},{0: 0, 1: 0, 2: 1}))]
-    >>> S == [list2vec(v) for v in [[1,0,0],[0,1,0],[0,0,1]]]
-    True
-    >>> B == [list2vec(v) for v in [[1,1,0],[0,1,1],[1,0,1]]]
-    True
+    Example:
+        >>> # This is how our morph works.  Yours may yield different results.
+        >>> # Note: Make a copy of S to modify instead of modifying S itself.
+        >>> S = [list2vec(v) for v in [[1,0,0],[0,1,0],[0,0,1]]]
+        >>> B = [list2vec(v) for v in [[1,1,0],[0,1,1],[1,0,1]]]
+        >>> morph(S, B)
+        [(Vec({0, 1, 2},{0: 1, 1: 1, 2: 0}), Vec({0, 1, 2},{0: 1, 1: 0, 2: 0})), (Vec({0, 1, 2},{0: 0, 1: 1, 2: 1}), Vec({0, 1, 2},{0: 0, 1: 1, 2: 0})), (Vec({0, 1, 2},{0: 1, 1: 0, 2: 1}), Vec({0, 1, 2},{0: 0, 1: 0, 2: 1}))]
+        >>> S == [list2vec(v) for v in [[1,0,0],[0,1,0],[0,0,1]]]
+        True
+        >>> B == [list2vec(v) for v in [[1,1,0],[0,1,1],[1,0,1]]]
+        True
     '''
-    tmp=[]
-    for i,e in enumerate(B):
-        tmp.append((e,S[i]))
-    return tmp
+    eject_order = []
+    Sleft = S.copy() # What's left to eject that we haven't done already?
+    Sprime = S.copy()
+    for i in range(len(B)):
+      Sprime.append(B[i]) # inject B
+      for j in S: # now scan through S and look for something to remove.
+         if j in Sleft and is_superfluous(Sprime, j):
+            eject_order.append((B[i],j))
+            Sleft.remove(j) # remove it from future consideration
+            break # next B[i]!
+    return eject_order
 
 
 
 ## 4: (Problem 4) Row and Column Rank Practice
 # Please express each solution as a list of Vecs
 
-row_space_1 = [...]
-col_space_1 = [...]
+row_space_1 = [Vec({0, 1, 2}, {0:1 ,1:2, 2:0}), Vec({0, 1, 2}, {0:0, 1:2, 2:1})]
+col_space_1 = [Vec({0, 1}, {0:1, 1:0}), Vec({0, 1}, {0:2, 1:2})]
 
-row_space_2 = [...]
-col_space_2 = [...]
+row_space_2 = [Vec({0, 1, 2, 3}, {0:1, 1:4, 2:0, 3:0}), Vec({0, 1, 2, 3}, {0:0, 1:2, 2:2, 3:0}), Vec({0, 1, 2, 3}, {0:0, 1:0, 2:1, 3:1})]
+col_space_2 = [Vec({0, 1, 2}, {0:1, 1:0, 2:0}), Vec({0, 1, 2}, {0:4, 1:2, 2:0}), Vec({0, 1, 2}, {0:0, 1:2, 2:1})]
 
-row_space_3 = [...]
-col_space_3 = [...]
+row_space_3 = [Vec({0}, {0:1})] 
+col_space_3 = [Vec({0, 1, 2}, {0:1, 1:2, 2:3})]
 
-row_space_4 = [...]
-col_space_4 = [...]
-
-
+row_space_4 = [Vec({0, 1}, {0:2, 1:1}), Vec({0, 1}, {0:3, 1:4})]
+col_space_4 = [Vec({0, 1, 2}, {0:1, 1:2, 2:3}), Vec({0, 1, 2}, {0:0, 1:1, 2:4})]
 
 ## 5: (Problem 5) Subset Basis
 def subset_basis(T):
@@ -120,14 +131,17 @@ def subset_basis(T):
         True
         >>> is_independent(sb)
         True
-
         >>> D = {'a','b','c','d'}
         >>> c0, c1, c2, c3, c4 = Vec(D,{'d': one, 'c': one}), Vec(D,{'d': one, 'a': one, 'c': one, 'b': one}), Vec(D,{'a': one}), Vec(D,{}), Vec(D,{'d': one, 'a': one, 'b': one})
         >>> subset_basis({c0,c1,c2,c3,c4}) == {c0,c1,c2,c4}
-True
+        True
     '''
-    pass
-
+    S = set()
+    for i in T:
+       S.add(i)
+       if not(is_independent(S)):
+          S.discard(i)
+    return S
 
 
 ## 6: (Problem 6) Superset Basis Lemma in Python
@@ -150,12 +164,15 @@ def superset_basis(C, T):
         >>> sb = superset_basis({a0, a3}, {a0, a1, a2})
         >>> a0 in sb and a3 in sb
         True
-        >>> is_independent(sb)
-        True
         >>> all(x in [a0,a1,a2,a3] for x in sb)
         True
     '''
-    pass
+    tmp = C.copy()
+    for v in T:
+        tmp.add(v)
+        if is_superfluous(tmp, v):
+            tmp.discard(v)
+    return tmp
 
 
 
@@ -186,7 +203,7 @@ def my_is_independent(L):
         >>> L == [Vec(D,{0: 1}), Vec(D,{1: 1}), Vec(D,{2: 1}), Vec(D,{0: 1, 1: 1, 2: 1}), Vec(D,{0: 1, 1: 1}), Vec(D,{1: 1, 2: 1})]
         True
     '''
-    pass
+    return len(L) == rank(L)
 
 
 
@@ -206,7 +223,7 @@ def my_rank(L):
         >>> my_rank([list2vec(v) for v in [[1,1,1],[2,2,2],[3,3,3],[4,4,4],[123,432,123]]])
         2
     '''
-    pass
+    return len(subset_basis(L))
 
 
 
@@ -233,8 +250,10 @@ def direct_sum_decompose(U_basis, V_basis, w):
         >>> (v - V_matrix*solve(V_matrix, v)).is_almost_zero()
         True
     '''
-    pass
-
+    W = solve(coldict2mat(U_basis + V_basis), w)
+    Wu = Vec(set(range(len(U_basis))), { i : W[i] for i in range(len(U_basis))})
+    Wv = Vec(set(range(len(V_basis))), { i : W[i + len(U_basis)] for i in range(len(V_basis))})
+    return( coldict2mat(U_basis) * Wu, coldict2mat(V_basis) * Wv)
 
 
 ## 10: (Problem 10) Is Invertible Function
@@ -251,8 +270,13 @@ def is_invertible(M):
     >>> is_invertible(M1)
     False
     '''
-    pass
-
+    Rows=[]
+    Cols=[]
+    for k,v in mat2rowdict(M).items():
+        Rows.append(v)
+    for k,v in mat2coldict(M).items():
+        Cols.append(v)
+    return (rank(Rows) == rank(Cols) and my_is_independent(Rows) and my_is_independent(Cols))
 
 
 ## 11: (Problem 11) Inverse of a Matrix over GF(2)
@@ -270,8 +294,7 @@ def find_matrix_inverse(A):
         >>> find_matrix_inverse(M2) == Mat(M2.D, {(0, 1): one, (1, 0): one, (2, 2): one})
         True
     '''
-    pass
-
+    return coldict2mat([solve(A, Vec(A.D[0], {i: one})) for i in A.D[0]])
 
 
 ## 12: (Problem 12) Inverse of a Triangular Matrix
@@ -279,15 +302,28 @@ def find_triangular_matrix_inverse(A):
     '''
     Supporting GF2 is not required.
 
-    Input:
-        - A: an upper triangular Mat with nonzero diagonal elements
-    Output:
-        - Mat that is the inverse of A
-    
+    Input: - A: an upper triangular Mat with nonzero diagonal elements
+    Output: - Mat that is the inverse of A
     Example:
         >>> A = listlist2mat([[1, .5, .2, 4],[0, 1, .3, .9],[0,0,1,.1],[0,0,0,1]])
         >>> find_triangular_matrix_inverse(A) == Mat(({0, 1, 2, 3}, {0, 1, 2, 3}), {(0, 1): -0.5, (1, 2): -0.3, (3, 2): 0.0, (0, 0): 1.0, (3, 3): 1.0, (3, 0): 0.0, (3, 1): 0.0, (2, 1): 0.0, (0, 2): -0.05000000000000002, (2, 0): 0.0, (1, 3): -0.87, (2, 3): -0.1, (2, 2): 1.0, (1, 0): 0.0, (0, 3): -3.545, (1, 1): 1.0})
         True
     '''
-    pass
+    rowdict = mat2rowdict(A)
+    return coldict2mat([triangular_solve([rowvalue for rowvalue in rowdict.values()], [k for k in rowdict.keys()], Vec(A.D[0], {i: 1})) for i in A.D[0]])
 
+
+
+# S = [list2vec(v) for v in [[2,4,0],[1,0,3],[0,4,4],[1,1,1]]]
+# B = [list2vec(v) for v in [[1,0,0],[0,1,0],[0,0,1]]]
+# for (z,w) in morph(S,B):
+#    print("injecting ",z)
+#    print("ejecting ",w)
+#    print()
+
+B = [Vec({0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19},{0: one, 1: one, 3: one, 5: one, 19: one, 8: one, 9: one, 10: one, 12: one, 15: one}), Vec({0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19},{0: one, 2: one, 3: one, 4: one, 8: one, 9: one, 10: one, 11: one, 12: one, 13: one, 14: one, 16: one}), Vec({0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19},{0: one, 1: one, 5: one, 6: one, 7: one, 8: one, 11: one, 13: one, 14: one, 15: one, 16: one, 17: one, 19: one}), Vec({0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19},{16: one, 2: one, 5: one, 6: one, 7: one, 9: one, 10: one, 11: one, 12: one, 18: one, 14: one}), Vec({0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19},{0: one, 1: one, 2: one, 19: one, 4: one, 6: one, 8: one, 9: one, 10: one, 13: one, 14: one}), Vec({0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19},{1: one, 3: one, 4: one, 5: one, 9: one, 10: one, 11: one, 14: one}), Vec({0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19},{16: one, 1: one, 18: one, 3: one, 4: one, 5: one, 17: one, 7: one, 10: one, 12: one, 13: one})]
+S=[Vec({0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19},{16: one, 1: one, 2: one, 19: one, 4: one, 5: one, 11: one, 13: one, 14: one, 15: one}), Vec({0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19},{0: one, 1: one, 2: one, 8: one, 9: one, 10: one, 12: one, 13: one, 15: one, 17: one, 18: one, 19: one}), Vec({0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19},{16: one, 17: one, 4: one, 6: one, 8: one, 9: one, 10: one, 12: one}), Vec({0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19},{16: one, 17: one, 18: one, 7: one, 9: one, 11: one, 12: one, 13: one, 14: one}), Vec({0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19},{0: one, 2: one, 3: one, 5: one, 6: one, 7: one, 8: one, 10: one, 11: one, 12: one, 13: one, 17: one, 18: one, 19: one}), Vec({0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19},{19: one, 17: one, 3: one, 4: one, 6: one, 7: one, 8: one, 9: one, 10: one, 13: one}), Vec({0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19},{0: one, 19: one, 2: one, 3: one, 5: one, 6: one, 8: one, 11: one, 13: one}), Vec({0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19},{0: one, 1: one, 3: one, 4: one, 5: one, 6: one, 17: one, 16: one, 19: one, 15: one}), Vec({0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19},{0: one, 1: one, 4: one, 5: one, 7: one, 8: one, 11: one, 12: one, 13: one, 16: one, 18: one, 19: one}), Vec({0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19},{1: one, 2: one, 3: one, 4: one, 5: one, 6: one, 7: one, 9: one, 10: one, 12: one, 15: one, 17: one, 19: one})]
+for (z,w) in morph(S,B):
+   print("injecting ",z)
+   print("ejecting ",w)
+   print()
