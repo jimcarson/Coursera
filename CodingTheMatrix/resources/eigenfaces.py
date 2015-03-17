9fa06507388857cbe49a28a6f42ff9ffd5d15e0b
Jim Carson <jim.carson@gmail.com>
2015-03-17 14:33:30 -0700

    Reorganizing

---
+++ CodingTheMatrix/resources/eigenfaces.py
@@ -0,0 +1,13 @@
+<html>
+<head><title>403 Forbidden</title></head>
+<body>
+<h1>403 Forbidden</h1>
+<ul>
+<li>Code: AccessDenied</li>
+<li>Message: Access Denied</li>
+<li>RequestId: 5329C19C1E1C0D14</li>
+<li>HostId: tfFDSieA0kp1Hwd48tuirYvlYFKitN/Wbfg85uTipw253CwGwZTVVKmonCC9tl5NPhpp8Sfa5KA=</li>
+</ul>
+<hr/>
+</body>
+</html>
