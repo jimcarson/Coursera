971034b6dea899909a67937692a150a932d5a9e9
Jim Carson <jim.carson@gmail.com>
2015-03-17 18:31:23 -0700

    Added reverse function.

--- CodingTheMatrix/hw12_basis_problems/vecutil.py
+++ CodingTheMatrix/hw12_basis_problems/vecutil.py
@@ -14,3 +14,18 @@ def zero_vec(D):
     """Returns a zero vector with the given domain
     """
     return Vec(D, {})
+
+def vec2list(V):
+    """Given a Vec, return a list of field elements whose entry i is L[i]
+    >>> vec2list(Vec({0, 1, 2},{0: 10, 1: 20, 2: 30}))
+    [10, 20, 30]
+    """
+    return [V.f[i] for i in list(V.D)]
+
+def rvec2list(V,r):
+    """Given a Vec, return a list of field elements whose entry i is L[i]
+       but round the results to r digits.
+    >>> vec2list(Vec({0, 1, 2},{0: 10, 1: 20, 2: 30}))
+    [10, 20, 30]
+    """
+    return [round(V.f[i],r) for i in list(V.D)]
